ffce6a7a3d086a116fe7b5a7aaaecf7d59b6eeca
Juan B <juanbalderramo9@gmail.com>
2022-09-29 20:37:44 -0500

    proyecto-maven

---
+++ RomanNumerals/src/main/java/model/RomanNumber.java
@@ -0,0 +1,14 @@
+package model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RomanNumber {
+    private String symbol;
+    private  int value;
+
+}
+
